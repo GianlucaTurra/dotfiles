d07485b3806c93d43258f3f4e00342535cf65988
Gianluca Turra <gianluca.turra@sanmarcoinformatica.it>
2025-04-16 11:31:59 +0200

    Nvim: spelling check
    Spelling check for comments and prose in both english and italian

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,7 @@
 vim.opt.tabstop = 4 -- A tab character is 4 spaces
 vim.opt.shiftwidth = 4 -- Indentation level is 4 spaces
 vim.opt.expandtab = true -- Convert tabs to spaces
+
+-- Language spell check
+vim.opt.spell = true
+vim.opt.spelllang = "en,it"
