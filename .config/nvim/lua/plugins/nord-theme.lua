30190342950193203777f6bb3a5b7cd64fa8038c
GianlucaTurra <gianlucaturra1@gmail.com>
2025-05-15 11:48:44 +0200

    Cherry-pick theming

---
+++ .config/nvim/lua/plugins/nord-theme.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "dupeiran001/nord.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "nord",
+        },
+    },
+}
