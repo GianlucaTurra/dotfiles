82f0f5cf48b81719e070734f07c46419b1fd9a1d
GianlucaTurra <gianlucaturra1@gmail.com>
2025-10-30 10:59:52 +0100

    Nvim: rose-pine transparency

--- .config/nvim/lua/plugins/rose-pine.lua
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -2,6 +2,19 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
+        require("rose-pine").setup({
+            enable = {
+                terminal = true,
+            },
+            styles = {
+                transparency = true,
+            },
+        })
+        vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+        vim.api.nvim_set_hl(0, "PmenuSel", { bg = "none" })
+        vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "CmpDocumentation", { bg = "none" })
         vim.cmd("colorscheme rose-pine")
     end,
 }
