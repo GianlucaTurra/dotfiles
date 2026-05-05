b4c5ccd4e192c1025c70243c95d6c1a4c494db19
GianlucaTurra <gianlucaturra1@gmail.com>
2026-05-05 23:45:29 +0200

    Nvim: django lsp and linter

--- .config/nvim/lua/plugins/django.lua
+++ .config/nvim/lua/plugins/django.lua
@@ -7,6 +7,23 @@ return {
             },
         },
     },
+    {
+        "mfussenegger/nvim-lint",
+        opts = {
+            linters_by_ft = {
+                djangohtml = { "djlint" },
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        ---@class PluginLspOpts
+        opts = {
+            servers = {
+                djlsp = {},
+            },
+        },
+    },
     require("luasnip").filetype_extend("python", { "django" }),
     require("luasnip").filetype_extend("django-html", { "djangohtml", "html" }),
 }
